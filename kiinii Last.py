dd96035ff76f10cdbfebc50a42fbeb7c26b87469
JohnserfSeed <40727745+Johnserf-Seed@users.noreply.github.com>
2020-10-05 21:46:13 +0800

    优化了代码
    使用类进行操作

--- kiinii Last.py
+++ kiinii Last.py
@@ -1,86 +1,87 @@
-from bs4 import BeautifulSoup
+# -*- coding:utf-8 -*- 
+#Time: 2020/10/05/21:22
+#Author: JohnserfSeed
+#Origin： XueMian
+#GitHub: https://github.com/Johnserf-Seed;https://github.com/xuemian168
+
+from lxml import etree
 import requests
 
-def Photo():
-    def get_html(url):
-        headers = {
+class kiinii:
+
+    #文件下标
+    index = 1
+
+    #资源地址
+    blog_url = 'https://kiinii.com/blog/'
+    video_url = 'https://www.kiinii.com/videoclass/'
+
+    #UA字典
+    headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-            'Referer': 'https://kiinii.com/blog/101979/',
+            'Host': 'kiinii.com',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Connection': 'keep-alive',
             'Accept-Encoding': 'gzip,deflate,br',
             'Accept-Language': 'zh,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7'
         }
-        response = requests.get(url, headers=headers)
-        html = response.text
-        return html
-    n = 0
-    art = input('输入文章号:')
-    arturl = 'https://kiinii.com/blog/' + art + '/'
-    cdn = 'https://sogoke-photo-image.kiinii.com/media/photos/'
-    soup = BeautifulSoup(get_html(arturl), 'lxml')
-    try:
-        title = soup.title.text
-        print(title)
-    except AttributeError as e:
-        return "标签异常"
-    for data1 in soup.find_all('div', class_='blog-content sogoke-text-area mb40'):
-        for data2 in data1.find_all('img'):
-            data1 = data2.get('data-suid')
-            downurl = cdn + data1
-            print("正在下载",str(n+1))
-            n+=1
-            try:
-                r = requests.get(downurl)
-                with open('E:/test/'+(str(n + 1)) + '.jpg', 'wb') as code:
-                    code.write(r.content)
-                    n += 1
-                    print('下载完成')
-            except:
-                pass
-def formatFloat(num):
-    return '{:.2f}'.format(num)
-def Vedio():
-    def get_html(url):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
-            'Referer': 'https://kiinii.com/blog/101979/',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Connection': 'keep-alive',
-            'Accept-Encoding': 'gzip,deflate,br',
-            'Accept-Language': 'zh,en-US;q=0.9,en;q=0.8,zh-CN;q=0.7'
-        }
-        response = requests.get(url, headers=headers)
-        html = response.text
-        return html
-    n = 1
-    n += 1
-    art = input('输入文章号:')
-    path = input('请输入保存路径:(如E:/test/,输入local为脚本所在目录)')
-    if path == 'local':
-        path = './'
-    arturl = 'https://kiinii.com/videoclass/' + art + '/'
-    soup = BeautifulSoup(get_html(arturl), 'lxml')
-    for data1 in soup.find_all('div', class_='main-video w1000'):
-        for data2 in data1.find_all('video'):
-            data1 = data2.get('src')
-            downurl = data1
-            try:
-                print('视频正在下载 可能比较慢 路径为：', path)
-                r = requests.get(downurl)
-                with open(path+(str(n + 1)) + '.mp4', 'wb') as code:
-                    code.write(r.content)
-                    n += 1
-            except:
-                print('出现错误 该页面没有找到视频或其他错误')
-                pass
-def choice():
-    which = input('\n输入1启动抓图片 \n输入2启动抓视频\n请输入：')
-    if which == '1':
-        Photo()
-    if which == '2':
-        Vedio()
-    else:
-        print('\n输入有误 重新选择\n输入有误 重新选择\n输入有误 重新选择')
-        choice()
-choice()
+
+    def __init__(self,sid):
+        self.down_img(rid)
+
+    #图片下载
+    def down_img(self,rid):
+        try:
+            r = requests.get(self.blog_url + str(rid),headers=self.headers)
+            res = etree.HTML(r.text)
+            img_url=res.xpath('//img[@class="uploaded_photo"]/@src')
+            if(img_url==[]):
+                self.down_mp4(rid)
+        except:
+            #print("此资源为视频资源")
+            pass
+        else:
+            for img in img_url:
+                try:
+                    print("正在下载...",img)
+                    r = requests.get(img)
+                    #img,'.jpg'
+                    with open(r'.\\img' + (str(self.index)) + '.jpg', 'wb') as code:
+                        code.write(r.content)
+                        self.index += 1
+                except:
+                    input(print("下载出错!"))
+                    exit(0)
+            input(print('下载完成!!'))
+            exit(0)
+        return
+    
+    #视频下载
+    def down_mp4(self,rid):
+        try:
+            r = requests.get(self.video_url + str(rid),headers=self.headers)
+            res = etree.HTML(r.text)
+            mp4_url=res.xpath('//video[@class="sogoke-video video-js"]/@src')
+            if(mp4_url==[]):
+                self.down_mp4(rid)
+        except:
+            #print("此资源为图片资源")
+            pass
+        else:
+            for mp4 in mp4_url:
+                try:
+                    print("正在下载...",mp4)
+                    r = requests.get(mp4)
+                    with open(r'.\\video' + str((self.index)) + '.mp4', 'wb') as code:
+                        code.write(r.content)
+                        self.index += 1
+                except:
+                    input(print("下载出错!"))
+                    exit(0)
+            input(print('下载完成!!'))
+            exit(0)
+        return
+
+if __name__ == "__main__":
+    rid=input("输入文章ID:")
+    kiinii(rid)
